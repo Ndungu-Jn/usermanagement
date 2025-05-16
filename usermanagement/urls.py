f584b4db6f1aeed8340bc79fa6a1ab797dfe7dd0
John Ndungu <ndungulink@gmail.com>
2025-05-16 15:23:43 +0300

    Postman collection

--- usermanagement/urls.py
+++ usermanagement/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.http import HttpResponse
 
 def home_view(request):
-    return HttpResponse("Django User Management API - Access API endpoints at /api/")
+    return HttpResponse(" User Management - Access API endpoints at /api/users/")
 
 urlpatterns = [
     path('', home_view, name='home'),
